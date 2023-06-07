6158ca0b4a31887a4b4900f62e8a5528c8590993
Alexander Korolev <alexander.korolev.germany@gmail.com>
2023-06-07 20:35:38 +0200

    added target_include_directories command

--- lib/src/doc/command/project/target_include_directories.rs
+++ lib/src/doc/command/project/target_include_directories.rs
@@ -12,13 +12,31 @@ use crate::{
 #[cmake(pkg = "crate")]
 pub struct TargetIncludeDirectories<'t> {
     #[cmake(positional)]
-    pub name: Token<'t>,
+    pub target: Token<'t>,
+    pub system: bool,
+    pub mode: Option<Mode>,
+    pub directories: Option<Vec<Directory<'t>>>,
 }
 
 impl<'t> ToCommandScope for TargetIncludeDirectories<'t> {
     fn to_command_scope(&self) -> CommandScope {
         CommandScope::Project
     }
+}
+
+#[derive(CMake, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[cmake(pkg = "crate")]
+pub enum Mode {
+    After,
+    Before,
+}
+
+#[derive(CMake, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[cmake(pkg = "crate", transparent)]
+pub enum Directory<'t> {
+    Interface(Vec<Token<'t>>),
+    Public(Vec<Token<'t>>),
+    Private(Vec<Token<'t>>),
 }
 
 #[cfg(test)]
@@ -36,11 +54,43 @@ mod tests {
         let doc = Doc::from(cmakelists);
         assert_eq!(
             doc.commands(),
-            Ok(vec![Command::TargetIncludeDirectories(Box::new(
-                TargetIncludeDirectories {
-                    name: token(b"name"),
-                }
-            )),])
+            Ok(vec![
+                Command::TargetIncludeDirectories(Box::new(TargetIncludeDirectories {
+                    target: token(b"name"),
+                    system: false,
+                    mode: None,
+                    directories: None,
+                })),
+                Command::TargetIncludeDirectories(Box::new(TargetIncludeDirectories {
+                    target: token(b"mylib"),
+                    system: false,
+                    mode: None,
+                    directories: Some(vec![Directory::Public(tokens_vec([
+                        b"$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include/mylib>",
+                        b"$<INSTALL_INTERFACE:include/mylib>",
+                    ]))]),
+                })),
+                Command::TargetIncludeDirectories(Box::new(TargetIncludeDirectories {
+                    target: token(b"mylib"),
+                    system: true,
+                    mode: Some(Mode::Before),
+                    directories: Some(vec![Directory::Public(tokens_vec([
+                        b"$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include/mylib>",
+                        b"$<INSTALL_INTERFACE:include/mylib>",
+                    ]))]),
+                })),
+                Command::TargetIncludeDirectories(Box::new(TargetIncludeDirectories {
+                    target: token(b"mylib"),
+                    system: false,
+                    mode: Some(Mode::After),
+                    directories: Some(vec![
+                        Directory::Interface(vec![token(
+                            b"$<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include/mylib>"
+                        )]),
+                        Directory::Private(vec![token(b"$<INSTALL_INTERFACE:include/mylib>")])
+                    ]),
+                })),
+            ])
         )
     }
 }
